2756bae42ce92e91be4cae3feccb4e220c66683b
madang <madang@ttu.ee>
2021-02-16 09:48:54 +0200

    level 3

--- src/test/java/ee/itcollage/level3/LargestNumbestTest.java
+++ src/test/java/ee/itcollage/level3/LargestNumbestTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class LargestNumbestTest {
+public class  LargestNumbestTest {
 
     @Test
     public void pick_largest_of_2() {
@@ -16,5 +16,12 @@ public class LargestNumbestTest {
     public void pick_largest_of_3() {
         assertEquals(1, LargestNumber.pickLargest(1, 0, -1));
         assertEquals(1, LargestNumber.pickLargest(0, 1, -1));
+    }
+
+    @Test
+    public void pick_Absolute() {
+        assertEquals(3, AbsoluteNumber.makeAbsolute(-3));
+        assertEquals(13, AbsoluteNumber.makeAbsolute(-13));
+        assertEquals(7, AbsoluteNumber.makeAbsolute(7));
     }
 }
