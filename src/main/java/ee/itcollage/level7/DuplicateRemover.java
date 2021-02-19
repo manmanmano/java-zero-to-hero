1305873711eedf047f16129c870cf6cb68072e07
Mariano D'Angelo <madang@ttu.ee>
2021-02-19 17:14:55 +0200

    level 7 done

--- src/main/java/ee/itcollage/level7/DuplicateRemover.java
+++ src/main/java/ee/itcollage/level7/DuplicateRemover.java
@@ -1,11 +1,22 @@
 package ee.itcollage.level7;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class DuplicateRemover {
 
     //todo fix tests
-    public static List<Integer> removeDuplicates(List<Integer> numbers){
-        return numbers;
+    public static List<Integer> removeDuplicates(List<Integer> numbers) {
+        return numbers.isEmpty() ? numbers : new ArrayList<>(new HashSet<>(numbers));
+
+//        same as:
+//        if(numbers.isEmpty()){
+//            return numbers;
+//        }
+//        HashSet<Integer> set = new HashSet<>(numbers);
+//        List<Integer> nodup = new ArrayList<>(set);
+//        return nodup;
+
     }
 }
